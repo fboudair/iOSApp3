d229b1925482e519de125e421ac622440ccd23cb
Louise Rennick <wddtechnologies@trios.com>
2025-02-19 18:04:27 -0500

    Initial Commit

---
+++ Chapter22/Chapter22App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter22App.swift
+//  Chapter22
+//
+//  Created by Louise Rennick on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct Chapter22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
